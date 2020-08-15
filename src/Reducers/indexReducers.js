2101c1843fc7ddf89c23cd27bb11c970ccc8ad90
lawpal86 <67480487+lawpal86@users.noreply.github.com>
2020-08-15 13:24:32 -0500

    minor changes

--- src/Reducers/indexReducers.js
+++ src/Reducers/indexReducers.js
@@ -1,8 +1,8 @@
 import {combineReducers} from 'redux';
-import  usersReducer  from './usersReducers';
+import  countriesReducer  from './countriesReducers';
 
 const rootReducer = combineReducers({
-    users: usersReducer,
+    countries: usersReducer,
 })
 
 export default rootReducer;
