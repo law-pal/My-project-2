a4488e43a4213107fcf42e9b113247e880e7d197
lawpal86 <67480487+lawpal86@users.noreply.github.com>
2020-08-15 13:20:37 -0500

    created store for redux

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import {Provider} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import rootReducer from './Reducers';
+
+const store = createStore(
+  rootReducer, composeWithDevTools(applyMiddleware(thunk))
+)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
