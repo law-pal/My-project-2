e1ad53946faa04edd80b9e0e9168e28e2b138f10
lawpal86 <67480487+lawpal86@users.noreply.github.com>
2020-08-16 17:35:21 -0500

    css update

--- src/Components/Countries.js
+++ src/Components/Countries.js
@@ -25,7 +25,6 @@ class Countries extends Component {
     render() {
         return(
             <div>
-                 <h1>Hello</h1>
                 {this.renderCountry()}
             </div>
         );
--- src/Components/Country.js
+++ src/Components/Country.js
@@ -5,11 +5,11 @@ const Country = (props) => {
        
     return (
         <div className='container'>
-            <div className='country'>
-                    <div className='country-header'>
-                    <img className='flags' src={props.nation.flag} alt='#'/>
+            <div className='card'>
+                    <div className='card-header'>
+                        <img className='flags' src={props.nation.flag} alt='#'/>
                     </div>
-                <div className='country-body'>
+                    <div className='card-body'>
                     <h2>{props.nation.name}</h2>
                     <p>
                         <strong>Population:</strong>
