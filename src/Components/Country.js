dc5f33a6c656563118d2e57c7bf53b517d51f01b
lawpal86 <67480487+lawpal86@users.noreply.github.com>
2020-08-16 14:36:51 -0500

    update

--- src/Components/Country.js
+++ src/Components/Country.js
@@ -4,9 +4,27 @@ import React from 'react';
 const Country = (props) => {
        
     return (
-        <div>
-           <h1> {props.nation.name}</h1> 
-            <p>{props.nation.region}</p>
+        <div className='container'>
+            <div className='country'>
+                <div className='country-header'>
+                    <img src={props.nation.flag} alt='#'/>
+                </div>
+                <div className='country-body'>
+                    <h2>{props.nation.name}</h2>
+                    <p>
+                        <strong>Population:</strong>
+                        {props.nation.population}
+                    </p>
+                    <p>
+                        <strong>Region:</strong>
+                        {props.nation.region}
+                    </p>
+                    <p>
+                        <strong>Capital:</strong>
+                        {props.nation.capital}
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
