7719bcf4b46284f6ed463c940f62b416948a5389
lawpal86 <67480487+lawpal86@users.noreply.github.com>
2020-08-17 00:15:28 -0500

    saved pictures

--- src/Components/Colombia.js
+++ src/Components/Colombia.js
@@ -8,9 +8,22 @@ class Colombia extends Component {
 
     render() {
         return(
-            <div>
-                Hello
+            <div className='container-united'>
+            <div className='yellow'>Left</div>
+            <main className='red'>Main</main>
+            <div className='green'>
+                <div className='green-child'>
+                    <div className='one'><h1>Lawrence</h1></div>
+                    <div className='two'>2</div>
+                    <div className='three'>3</div>
+                    <div className='four'>4</div>
+                    <div className='five'>5</div>
+                    <div className='six'>6</div>
+                    <div className='seven'>7</div>
+                    <div className='eight'>8</div>
+                </div>
             </div>
+        </div>
         );
     }
 }
