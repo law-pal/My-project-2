1770c740531888ee2fa1da36c1f8fe364ba9cc3c
lawpal86 <67480487+lawpal86@users.noreply.github.com>
2020-08-17 23:41:41 -0500

    update

--- src/Components/Mexico.js
+++ src/Components/Mexico.js
@@ -1,6 +1,12 @@
 import React, {Component} from 'react';
-
-
+import mexicoOne from '../project2-img/mexico-1.jpg'
+import mexicoTwo from '../project2-img/mexico-2.jpg'
+import mexicoThree from '../project2-img/mexico-3.jpg'
+import mexicoFour from '../project2-img/mexico-4.jpg'
+import mexicoFive from '../project2-img/mexico-5.jpg'
+import mexicoSix from '../project2-img/mexico-6.jpg'
+import mexicoSeven from '../project2-img/mexico-7.jpg'
+import mexicoEigth from '../project2-img/mexico-8.jpg'
 
 class Mexico extends Component {
 
@@ -8,19 +14,21 @@ class Mexico extends Component {
 
     render() {
         return(
-            <div className='container-united'>
-            <div className='yellow'>Left</div>
-            <main className='red'>Main</main>
-            <div className='green'>
-                <div className='green-child'>
-                    <div className='one'><h1>Lawrence</h1></div>
-                    <div className='two'>2</div>
-                    <div className='three'>3</div>
-                    <div className='four'>4</div>
-                    <div className='five'>5</div>
-                    <div className='six'>6</div>
-                    <div className='seven'>7</div>
-                    <div className='eight'>8</div>
+            <div className='container-states'>
+            <div className='yellow-states'>Left</div>
+            <main className='red-states'>
+                <div></div>
+            </main>
+            <div className='green-states'>
+                <div className='green-child-states'>
+                    <div className='one-states'><img src={mexicoOne}/></div>
+                    <div className='two-states'><img src={mexicoTwo}/></div>
+                    <div className='three-states'><img src={mexicoThree}/></div>
+                    <div className='four-states'><img src={mexicoFour}/></div>
+                    <div className='five-states'><img src={mexicoFive}/></div>
+                    <div className='six-states'><img src={mexicoSix}/></div>
+                    <div className='seven-states'><img src={mexicoSeven}/></div>
+                    <div className='eight-states'><img className='img-eight' src={mexicoEigth}/></div>
                 </div>
             </div>
         </div>
--- src/Components/Spain.js
+++ src/Components/Spain.js
@@ -1,4 +1,12 @@
 import React, {Component} from 'react';
+import spainOne from '../project2-img/spain-1.jpg'
+import spainTwo from '../project2-img/spain-2.jpg'
+import spainThree from '../project2-img/spain-3.jpg'
+import spainFour from '../project2-img/spain-4.jpg'
+import spainFive from '../project2-img/spain-5.jpg'
+import spainSix from '../project2-img/spain-6.jpg'
+import spainSeven from '../project2-img/spain-7.jpg'
+import spainEigth from '../project2-img/spain-8.jpg'
 
 
 
@@ -8,19 +16,21 @@ class Spain extends Component {
 
     render() {
         return(
-            <div className='container-united'>
-            <div className='yellow'>Left</div>
-            <main className='red'>Main</main>
-            <div className='green'>
-                <div className='green-child'>
-                    <div className='one'><h1>Lawrence</h1></div>
-                    <div className='two'>2</div>
-                    <div className='three'>3</div>
-                    <div className='four'>4</div>
-                    <div className='five'>5</div>
-                    <div className='six'>6</div>
-                    <div className='seven'>7</div>
-                    <div className='eight'>8</div>
+            <div className='container-states'>
+            <div className='yellow-states'>Left</div>
+            <main className='red-states'>
+                <div></div>
+            </main>
+            <div className='green-states'>
+                <div className='green-child-states'>
+                    <div className='one-states'><img src={spainOne}/></div>
+                    <div className='two-states'><img src={spainTwo}/></div>
+                    <div className='three-states'><img src={spainThree}/></div>
+                    <div className='four-states'><img src={spainFour}/></div>
+                    <div className='five-states'><img src={spainFive}/></div>
+                    <div className='six-states'><img src={spainSix}/></div>
+                    <div className='seven-states'><img src={spainSeven}/></div>
+                    <div className='eight-states'><img className='img-eight' src={spainEigth}/></div>
                 </div>
             </div>
         </div>
