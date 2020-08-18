bbfe6da5b0e7bc10e3d48c1bc2bb7fdfd31ba7eb
lawpal86 <67480487+lawpal86@users.noreply.github.com>
2020-08-18 09:39:09 -0500

    update

--- src/Components/unitedStates.js
+++ src/Components/unitedStates.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
-import usaOne from '../project2-img/usa-1.jpg'
-import usaTwo from '../project2-img/usa-2.jpg'
-import usaThree from '../project2-img/usa-3.jpg'
-import usaFour from '../project2-img/usa-4.jpg'
-import usaFive from '../project2-img/usa-5.jpg'
-import usaSix from '../project2-img/usa-6.jpg'
-import usaSeven from '../project2-img/usa-7.jpg'
-import usaEigth from '../project2-img/usa-8.jpg'
+import usaOne from '../project2-img/usa-1.jpg';
+import usaTwo from '../project2-img/usa-2.jpg';
+import usaThree from '../project2-img/usa-3.jpg';
+import usaFour from '../project2-img/usa-4.jpg';
+import usaFive from '../project2-img/usa-5.jpg';
+import usaSix from '../project2-img/usa-6.jpg';
+import usaSeven from '../project2-img/usa-7.jpg';
+import usaEigth from '../project2-img/usa-8.jpg';
+import connect from 'react-redux';
 
 
 class UnitedStates extends Component {
