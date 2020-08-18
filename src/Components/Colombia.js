f65460f5eb8e8df23c4b1addc29075bfd41a9b0b
lawpal86 <67480487+lawpal86@users.noreply.github.com>
2020-08-17 23:05:25 -0500

    update

--- src/Components/Colombia.js
+++ src/Components/Colombia.js
@@ -1,5 +1,12 @@
 import React, {Component} from 'react';
 import colombiaOne from '../project2-img/colombia-1.jpg'
+import colombiaTwo from '../project2-img/colombia-2.jpg'
+import colombiaThree from '../project2-img/colombia-3.jpg'
+import colombiaFour from '../project2-img/colombia-4.jpg'
+import colombiaFive from '../project2-img/colombia-5.jpg'
+import colombiaSix from '../project2-img/colombia-6.jpg'
+import colombiaSeven from '../project2-img/colombia-7.jpg'
+import colombiaEight from '../project2-img/colombia-8.jpg'
 
 
 class Colombia extends Component {
@@ -8,22 +15,24 @@ class Colombia extends Component {
 
     render() {
         return(
-            <div className='container-united'>
-            <div className='yellow'>Left</div>
-            <main className='red'>Main</main>
-            <div className='green-states'>
-                <div className='green-child-states'>
-                    <div className='one-states'><img src={colombiaOne}/></div>
-                    <div className='two-states'>2</div>
-                    <div className='three-states'>3</div>
-                    <div className='four-states'>4</div>
-                    <div className='five-states'>5</div>
-                    <div className='six-states'>6</div>
-                    <div className='seven-states'>7</div>
-                    <div className='eight-states'>8</div>
+            <div className='container-states'>
+                <div className='yellow-states'>Left</div>
+                <main className='red-states'>
+                    <div></div>
+                </main>
+                <div className='green-states'>
+                    <div className='green-child-states'>
+                        <div className='one-states'><img src={colombiaOne}/></div>
+                        <div className='two-states'><img src={usaTwo}/></div>
+                        <div className='three-states'><img src={usaThree}/></div>
+                        <div className='four-states'><img src={usaFour}/></div>
+                        <div className='five-states'><img src={usaFive}/></div>
+                        <div className='six-states'><img src={usaSix}/></div>
+                        <div className='seven-states'><img src={usaSeven}/></div>
+                        <div className='eight-states'><img className='miami' src={usaEigth}/></div>
+                    </div>
                 </div>
             </div>
-        </div>
         );
     }
 }
