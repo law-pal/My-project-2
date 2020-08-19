ca791f2abbc363259258d113bc495428aa79322d
lawpal86 <67480487+lawpal86@users.noreply.github.com>
2020-08-18 20:11:18 -0500

    few changes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -13,7 +13,7 @@ class Navbar extends Component {
        <Router>
           <nav>
             <div id="menu">
-                <div id="main"><p className='mainmenu'>MAINMENU</p></div>
+                <div id="main"><p className='mainmenu'>MAIN MENU</p></div>
                     <ul id='navbar'>
                       <li>
                         <Link className='navbar-link' to='/'>Countries</Link>
